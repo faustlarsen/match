c13b3074c21bfce9117166ee86f4ee95f92a5382
faustlarsen <faustlarsen@gmail.com>
2020-10-22 15:45:44 -0500

    add numeric value to results

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,49 @@
 $(document).ready(function() {
   $("#celebrity").submit(function(event) {
+    event.preventDefault();
+    const name = $("#name").val();
+    const music = $("#music").val();
+    const movies = $("#movies").val();
+    const books = $("#books").val();
+    const results = $("#boom");
+    let score = 0;
+  //Music condition 
+  if (music === "Heavy Metal") {
+    score += 2;
+  } else {
+    score += 1;
+  }
 
-let name = $("#name").val();
-let music = $("#music").val();
-let movies = $("#movies").val();
-let books = $("#books").val();
-let celeb
-  
+  //Movies condition 
+  if (movies === "Horror") {
+    score += 2;
+  } else {
+    score += 1;
+  }
+
+  //Books condition 
+  if (books === "Autobiography") {
+    score += 2;
+  } else {
+    score += 1;
+  }
+
+
+
+  if (score % 2 == 0) {
+    results.text("Pokemon");
+  } else {
+    results.text("Pikachu");
+  }
+
+
+  });
+});
+
+
+
+
+/*
   if (music === "Heavy Metal" && movies === "Horror" && books === "Autobiography") {
   celeb = "Darth Vader";
   } else if (music === "Indy" && movies === "Comedy" && books === "Fiction") {
@@ -18,6 +55,4 @@ let celeb
   $(".name").text(name);
   $("#celeb").text(celeb);
   $("#result").show();
-    event.preventDefault();
-  });
-});
+*/
